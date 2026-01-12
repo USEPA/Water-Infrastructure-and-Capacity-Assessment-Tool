9e80bb0bda483c4cf00e539f5da75558ff03ad2f
cneely01 <neely.colleen@epa.gov>
2026-01-12 14:23:13 -0600

    Add new configuration file to set values for widely used variables.

---
+++ R/CWS_Analysis/00_config.R
@@ -0,0 +1,6 @@
+# Values updated quarterly
+
+# Values updated annually
+## SRF
+DWSRF_Initial_Agreement_Date_Start <- as.Date("2015-07-01", "%Y-%m-%d")  # Start date for DWSRF initial agreements to include in analysis
+DWSRF_Initial_Agreement_Date_End <- as.Date("2025-06-30","%Y-%m-%d") # End date for DWSRF initial agreements to include in analysis
